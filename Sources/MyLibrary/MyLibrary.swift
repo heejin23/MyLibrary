22bc8843f9f08d0ccda22d71e81b6629c949987d
전희진/선임연구원/MF Project(heejin.jun@lge.com) <heejin.jun@lge.com>
2023-12-15 15:27:45 +0900

    second release

--- Sources/MyLibrary/MyLibrary.swift
+++ Sources/MyLibrary/MyLibrary.swift
@@ -1,2 +1,10 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public class MyLibrary {
+    public func test () {
+        print("start")
+    }
+}
